906dc824bc9c5bce1e8bc75eabf69680f84f2ee0
devs1man <devikamanoharan.6@gmail.com>
2026-06-30 19:48:05 +0530

    CRUD complete

--- backend/app/routes/student.py
+++ backend/app/routes/student.py
@@ -68,3 +68,24 @@ def update_student(
     db.commit()
     db.refresh(db_student)
     return db_student
+
+@router.delete("/students/{student_id}")
+def delete_student(
+    student_id: int,
+    db: Session=Depends(get_db)
+):
+    db_student = db.query(Student).filter(
+        Student.id == student_id
+    ).first()
+
+    if db_student is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    db.delete(db_student)
+    db.commit()
+
+    return{
+        "message":"Student deleted successfully"
+    }
