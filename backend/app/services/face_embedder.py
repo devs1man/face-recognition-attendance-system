862e4f34b7bbacc52902cc69ddbc2cf8583333ed
devs1man <devikamanoharan.6@gmail.com>
2026-07-04 22:57:51 +0530

    face embedder updated

--- backend/app/services/face_embedder.py
+++ backend/app/services/face_embedder.py
@@ -1,10 +1,12 @@
 from app.services.ai_manager import face_detector
 
 class FaceEmbedder:
-    def get_embedding(self, face):
-        faces = face_detector.detect_faces(face)
 
-        if len(faces)!=1:
+    def get_embedding(self, frame):
+
+        faces = face_detector.detect_faces(frame)
+
+        if len(faces) != 1:
             return None
-        
+
         return faces[0].embedding
