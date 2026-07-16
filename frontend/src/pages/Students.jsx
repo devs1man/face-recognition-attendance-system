c783571e236452fd82018c90273472b1fa2777c0
devs1man <devikamanoharan.6@gmail.com>
2026-07-16 09:12:36 +0530

    Integrate add student modal into students page

--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 
 import DashboardLayout from "../layouts/DashboardLayout";
 import StudentTable from "../components/students/StudentTable";
-
+import StudentModal from "../components/students/StudentModal";
+import StudentForm from "../components/students/StudentForm";
 import { getStudents } from "../api/studentApi";
 
 function Students() {
   const [students, setStudents] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     async function loadStudent() {
@@ -24,12 +26,22 @@ function Students() {
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Students</h1>
 
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-blue-600 text-white px-5 py-2   rounded-lg hover:bg-blue-700"
+        >
           + Add Student
         </button>
       </div>
 
       <StudentTable students={students} />
+      <StudentModal
+        isOpen={isModalOpen}
+        title="Add Student"
+        onCLose={() => setIsModalOpen(false)}
+      >
+        <StudentForm />
+      </StudentModal>
     </DashboardLayout>
   );
 }
